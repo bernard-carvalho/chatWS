d878828c0df9a23a3b3b299a6a22b7dd95827996
bernard <bernard.carvalho@ifto.edu.br>
2022-09-20 11:15:05 -0300

    login feito

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 //----
 $(
     ()=>{
+        let username
         const socket = io()
         $("#form1").on("submit",()=>{
-            socket.emit("chat msg", $("#msg").val())
+            if(username)
+                socket.emit("chat msg", $("#msg").val())
+            else{
+                username=$("#msg").val()
+                socket.emit("login", username)}
             return false})
         socket.on("chat msg", msg => $("#messages").append($("<li>").text(msg)))})
--- server.js
+++ server.js
@@ -17,8 +17,14 @@ app.get("/", ( _ , res) => res.sendFile(`${__dirname}/index.html`))
 
 serverSocket.on("connect", socket => {
     console.log(`Cliente ${socket.id} conectou`)
+    let username
+    socket.on("login", msg => {
+        username=msg;
+        console.log(`Usuario logado de de ${socket.id}: ${username}`)
+        serverSocket.emit("chat msg",`Usuario logado: ${username}`)
+        })
     socket.on("chat msg", msg => {
         console.log(`Msg recebida de ${socket.id}: ${msg}`)
-        serverSocket.emit("chat msg",`Msg recebida de ${socket.id}: ${msg}`)
+        serverSocket.emit("chat msg",`Msg enviada por ${username}: ${msg}`)
         })
 })
