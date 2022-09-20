64e171cb4abb6cce0551a7e4e239b65866d2a657
bernard <bernard.carvalho@ifto.edu.br>
2022-09-20 10:00:15 -0300

    commit socket

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express")
 const { Http2ServerRequest } = require("http2")
 const app = express()
 
+
 const http = require("http").Server(app)
 const PORT = 8000
+
+const serverSocket = require("socket.io")(http)
 
 http.listen(PORT, () => console.log(`Servidor Iniciado em ${PORT}`))
 
 app.get("/", ( _ , res) => res.sendFile(`${__dirname}/index.html`))
 
-
+serverSocket.on("connect", socket => {
+    console.log(`Cliente ${socket.id} conectou`);
+})
