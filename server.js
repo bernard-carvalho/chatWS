861976c2983deb5ade0e72b2da3f2e8834ee2ed6
Bernard Silva Araujo Wermuth de Carvalho <100189276+bernard-carvalho@users.noreply.github.com>
2022-09-27 08:48:42 -0300

    Update server.js
    commit da porta do heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.static("public"))
 
 const http = require("http").Server(app)
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 const serverSocket = require("socket.io")(http)
 
